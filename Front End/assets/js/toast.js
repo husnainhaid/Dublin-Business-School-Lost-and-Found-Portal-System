5c9d21564a1fb0c3c151655baccd5292604831db
Husnain <husnainhaider1218@gmail.com>
2025-11-26 13:49:59 +0000

    Add toast UI with fade-in and timeout

---
+++ Front End/assets/js/toast.js
@@ -0,0 +1,46 @@
+class ToastManager {
+  constructor() {
+    this.container = null;
+    this.toasts = [];
+    this.init();
+  }
+
+
+  init() {
+    
+    if (!document.querySelector('.toast-container')) {
+      this.container = document.createElement('div');
+      this.container.className = 'toast-container';
+      document.body.appendChild(this.container);
+    } else {
+      this.container = document.querySelector('.toast-container');
+    }
+  }
+
+  /**
+   
+   * @param {string} message 
+   * @param {string} type 
+   * @param {number} duration 
+   * @param {string} title 
+   */
+  show(message, type = 'info', duration = 4000, title = '') {
+    const toast = this.createToast(message, type, duration, title);
+    this.container.appendChild(toast);
+    this.toasts.push(toast);
+
+    
+    setTimeout(() => {
+      toast.style.opacity = '1';
+    }, 10);
+
+    
+    if (duration > 0) {
+      setTimeout(() => {
+        this.dismiss(toast);
+      }, duration);
+    }
+
+    return toast;
+  }
+}
