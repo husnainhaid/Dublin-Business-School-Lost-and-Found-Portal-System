50e68dbb6a252638339ae1c69291b756fd29ce71
Husnain <husnainhaider1218@gmail.com>
2025-11-28 15:09:52 +0000

    add file of adminlogin and few styling in css  that is code from ai  and in admin.js function calll adminlogin form

---
+++ Front End/assets/js/admin.js
@@ -0,0 +1,8 @@
+(function(){
+  const form = document.getElementById('adminLoginForm');
+  form?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(form);
+
+     });
+})();
