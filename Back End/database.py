32956a89246e47316b846193e86be95186d7d36c
Husnain <husnainhaider1218@gmail.com>
2025-12-05 19:25:51 +0000

    Updated database.py to use absolute path for DB_PATH  within my attributes

--- Back End/database.py
+++ Back End/database.py
@@ -1,23 +1,37 @@
 import sqlite3
 import os
 
-
-DB_PATH = os.path.join("instance", "items.db")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "instance", "items.db")
 
 
 def get_db():
-    
+    """Implement changes code to my database  ."""
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row  
     return conn
 
 
 def create_tables():
-    
+    """Create the items table if it doesn't already exist."""
     conn = get_db()
     cur = conn.cursor()
 
     cur.execute("""
-        
+        CREATE TABLE IF NOT EXISTS items (
+            item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_name TEXT NOT NULL,
+            item_name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            location TEXT NOT NULL,
+            date_lost TEXT NOT NULL,
+            category TEXT NOT NULL,
+            student_email TEXT NOT NULL,
+            phone_number TEXT NOT NULL,
+            status TEXT DEFAULT 'UNCLAIMED',
+            image TEXT
         )
     """)
+
+    conn.commit()
+    conn.close()
