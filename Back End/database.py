29977d61baa499b54815d755c11c066a998fbc14
Husnain <husnainhaider1218@gmail.com>
2025-12-05 19:20:43 +0000

    Refer code from github lost and found to understand

--- Back End/database.py
+++ Back End/database.py
@@ -6,4 +6,18 @@ DB_PATH = os.path.join("instance", "items.db")
 
 
 def get_db():
+    
     conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row  
+    return conn
+
+
+def create_tables():
+    
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+        
+        )
+    """)
