4601de750464639fdf5971cda943786daaeb2301
Husnain <husnainhaider1218@gmail.com>
2025-12-10 14:16:09 +0000

    refer code from template  edit_item and delete_item functions just to get idea to make the sequence within my structure and attributes

--- Back End/models.py
+++ Back End/models.py
@@ -37,5 +37,42 @@ def get_all_items():
 
     rows = cur.execute("SELECT * FROM items ORDER BY item_id DESC").fetchall()
 
-    conn.close()
-    return [dict(row) for row in rows]
+    items = [dict(row) for row in rows]
+    conn.close()  # Close connection after reading
+
+    return items
+
+#Referene from template:https://github.com/prince-c11/lost-found-management/blob/main/Lost-Found%20Management/app.py this will give undertstanding of def and other variables understadning with  edit
+
+
+def edit_item(item_id):
+    item = Item.query.get_or_404(item_id)
+
+    if request.method == 'POST':
+        item.name = request.form['name']
+        item.description = request.form['description']
+        item.category_id = request.form['category']
+        item.status = request.form['status']
+        item.date = request.form['date']
+        image_file = request.files['image_file']
+
+        if image_file:
+            image_filename = secure_filename(image_file.filename)
+            image_file.save(f'static/images/{image_filename}')
+            item.image_file = image_filename
+
+        db.session.commit()
+        flash('Item updated successfully.', 'success')
+        return redirect(url_for('home_page'))
+
+    categories = Category.query.all()
+    return render_template('edit_item.html', item=item, categories=categories)
+
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    flash('Item deleted successfully.', 'success')
+    return redirect(url_for('home_page'))
+
+@app.route('/item/<int:item_id>')
